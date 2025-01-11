4b46a5c5bed5b18f81bd7e6f11f4168a0b0617b7
Douglas Silva <douglasitpro@gmail.com>
2025-01-10 22:45:38 -0300

    Adicionando variveis inicializada com zero para temas de calculos

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ document.getElementById("calculo-form").addEventListener("submit", function (e){
     const feriasVencidas = parseInt(document.getElementById("ferias").value) || 0;
     const saldoFGTS = parseFloat(document.getElementById("fgts").value) || 0;
 
-
+    //temas para calculo
+    let avisoPrevio = 0;
+    let multaFGTS =0;
+    let feriasProporcionais = 0;
+    let decimoProporcional = 0;
 
 
 
