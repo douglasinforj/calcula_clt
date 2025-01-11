2ffab69ff91fe3420318c7f3bd65418de3cc6912
Douglas Silva <douglasitpro@gmail.com>
2025-01-11 07:41:53 -0300

    Adicionando função calcularINSS base 2024

--- js/script.js
+++ js/script.js
@@ -26,6 +26,35 @@ document.getElementById("calculo-form").addEventListener("submit", function (e){
     //Calcular 13º salário proporcional
     decimoProporcional = (salario / 12) * tempo;
 
+    //Regras mais detalhadas de calculos
+
+    //Calcular INSS
+    function calcularINSS(salario) {
+        let inss = 0;
+
+        if (salario <= 1412){
+            inss = salario * 0.075;
+        } else if (salario <= 2666.68){
+            inss = 1412 * 0.075 + (salario - 1412) * 0.09;
+        } else if (salario <= 4000.03) {
+            inss = 1412 * 0.075 + (2666.68 - 1412) * 0.09 + (salario - 2666.68) * 0.12;
+        } else if (salario <= 7786.02) {
+            inss = 1412 * 0.075 + 
+            (2666.68 - 1412) * 0.09 +
+            (4000.03 - 2666.68) * 0.12 +
+            (salario - 4000.03) * 0.14;
+        } else{
+            inss = 908.86  //Teto do Inss de contribuição. 
+        }
+        return inss;
+    }
+
+
+
+
+    //Calculo Descontos
+    const inss = calcularINSS(salario);
+
     //total
     const total = avisoPrevio + multaFGTS + feriasVencidas + feriasProporcionais + decimoProporcional;
 
@@ -37,6 +66,7 @@ document.getElementById("calculo-form").addEventListener("submit", function (e){
     <p><strong>Multa:</strong>R$ ${multaFGTS.toFixed(2)}
     <p><strong>Ferias Proporcionais:<strong>R$ ${feriasProporcionais.toFixed(2)}</p>
     <p><strong>13º Proporcional:</strong> R$ ${decimoProporcional.toFixed(2)}</p>
+    <p><strong>INSS</strong>R$ ${inss.toFixed(2)} </p>
     <p><strong>Total a Receber:</strong> R$ ${total.toFixed(2)}</p>
     `
 
